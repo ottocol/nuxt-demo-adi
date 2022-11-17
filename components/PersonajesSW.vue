718cb22fce597826e60ab6990cd52f9f8ed13450
ottocol <otto@dccia.ua.es>
2022-11-17 17:36:14 +0100

    Componente Personajes SW funcionando en Nuxt 3

---
+++ components/PersonajesSW.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Lista de personajes</h1>
+    <ul>
+      <li v-for="p in lista" :key="p.url">
+        {{ p.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default defineNuxtComponent({
+  fetchKey: 'personajes',
+  async asyncData() {
+    var datos = await $fetch('https://swapi.dev/api/people');
+    return {
+      lista: datos.results,
+    };
+  },
+});
+</script>
